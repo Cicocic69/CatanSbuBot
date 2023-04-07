9e1f78aa8ab98c9f45dced1ef7e392a5fc428458
Cicocic69 <122388110+Cicocic69@users.noreply.github.com>
2023-04-07 19:01:22 +0200

    Update testMotorias asd .ino

--- testMotorias asd .ino
+++ testMotorias asd .ino
@@ -1,29 +1,42 @@
+//libreria servo
+#include <Servo.h>
+
+//creazione oggetti servo
+Servo servoApriBraccio;
+Servo servoMuoviBraccio;
+
+//definizione pin servo
+#define servoApriBraccioPin 8
+#define servoMuoviBraccioPin 9
+
 //motore A
-const int enA = 3; // PWM 2
-const int in1 = 4;     // 22 
-const int in2 = 2;     // 23
-const int speedA = 255;
+const int enA = 2; // PWM
+const int in1 = 22;      
+const int in2 = 23;     
 
 //motore B
-const int enB = 5; // !!! QUESTO PWM FUNZIONA A 500, NON A 255 
-const int in3 = 8;
-const int in4 = 7;
-const int speedB = 500;
+const int enB = 3; // PWM
+const int in3 = 26; 
+const int in4 = 27; 
 
 //motore C
-const int enC = 6; // !!! QUESTO PWM FUNZIONA A 500, NON A 255
-const int in5 = 10;
-const int in6 = 9;
-const int speedC = 500;
+const int enC = 4; // PWM
+const int in5 = 28; 
+const int in6 = 29;  
 
 //motore D
-const int enD = 11; // PWM
-const int in7 = 13;
-const int in8 = 12;
-const int speedD = 255;
+const int enD = 7; // PWM
+const int in7 = 30; 
+const int in8 = 31; 
+
+//velocità
+const int speed255 = 255;
 
 void setup() {
   // put your setup code here, to run once:
+
+  servoApriBraccio.attach(servoApriBraccioPin);
+  servoMuoviBraccio.attach(servoMuoviBraccioPin);
 
   //motore A
   pinMode(enA, OUTPUT); 
@@ -61,7 +74,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  vaiAvanti();
+  /*vaiAvanti();
   vaiStop();
   delay(1000);
   vaiIndietro();
@@ -85,139 +98,196 @@ void loop() {
   diagDesIndietro();
   vaiStop();
   delay(1000);
+  ruotaSensoOrario();
+  vaiStop();
+  delay(1000);
+  ruotaSensoAntiorario();
+  vaiStop();
+  delay(1000);
+  aperturaBraccio();
+  delay(1000);
+  chiusuraBraccio();
+  delay(1000);
+  alzaBraccio();
+  delay(1000);
+  abbassaBraccio();
+  delay(1000);*/
 
+  vaiAvanti();
+  aperturaBraccio();
+  chiusuraBraccio();
+  alzaBraccio();
+  abbassaBraccio();
+  vaiIndietro();
+  
 }
 void vaiStop(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enB, speed255);    
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enC, speed255);    
   digitalWrite(in5, LOW);
   digitalWrite(in6, LOW);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, LOW);
   digitalWrite(in8, LOW);
   
   }
 void vaiAvanti(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enB, speed255);    
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enC, speed255);    
   digitalWrite(in5, HIGH);
   digitalWrite(in6, LOW);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, HIGH);
   digitalWrite(in8, LOW);
   delay(1000);
 }
 
 void vaiIndietro(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enB, speed255);    
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enC, speed255);    
   digitalWrite(in5, LOW);
   digitalWrite(in6, HIGH);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, LOW);
   digitalWrite(in8, HIGH);
   delay(1000);
 }
 
 void vaiADestra(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enB, speed255);    
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enC, speed255);    
   digitalWrite(in5, LOW);
   digitalWrite(in6, HIGH);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, HIGH);
   digitalWrite(in8, LOW);
   delay(1000);
 }
 
 void vaiASinistra(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enB, speed255);    
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+  analogWrite(enC, speed255);    
   digitalWrite(in5, HIGH);
   digitalWrite(in6, LOW);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, LOW);
   digitalWrite(in8, HIGH);
   delay(1000);
 }
 
 void diagDesAvanti(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, HIGH);
   digitalWrite(in8, LOW);
   delay(1000);
 }
 
 void diagSinAvanti(){
-  analogWrite(enB, speedA);
+  analogWrite(enB, speed255);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  analogWrite(enC, speedD);
+  analogWrite(enC, speed255);
   digitalWrite(in5, HIGH);
   digitalWrite(in6, LOW);
   delay(1000);
 }
 
 void diagDesIndietro(){
-  analogWrite(enA, speedA);
+  analogWrite(enA, speed255);
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, LOW);
   digitalWrite(in8, HIGH);
   delay(1000);
 }
 
 void diagSinIndietro(){
-  analogWrite(enB, speedA);
+  analogWrite(enB, speed255);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  analogWrite(enC, speedD);
+  analogWrite(enC, speed255);
   digitalWrite(in5, LOW);
   digitalWrite(in6, HIGH);
   delay(1000);
 }
 
-void ruotaDestra(){
-  analogWrite(enA, speedA);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  analogWrite(enB, speedB);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enC, speedC);    // !!! QUESTO PWM FUNZIONA A 500, NON A 255
+void ruotaSensoOrario(){
+  analogWrite(enA, speed255);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enB, speed255);    
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enC, speed255);    
   digitalWrite(in5, HIGH);
   digitalWrite(in6, LOW);
-  analogWrite(enD, speedD);
+  analogWrite(enD, speed255);
   digitalWrite(in7, HIGH);
   digitalWrite(in8, LOW);
+  delay(1000);
+}
+
+void ruotaSensoAntiorario(){
+  analogWrite(enA, speed255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enB, speed255);    
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enC, speed255);    
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, HIGH);
+  analogWrite(enD, speed255);
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, HIGH);
+  delay(1000);
+}
+
+void aperturaBraccio(){
+  servoApriBraccio.write(180);     //APERTO
+  delay(1000);
+}
+
+void chiusuraBraccio(){
+  servoApriBraccio.write(70);      //CHIUSO
+  delay(1000);
+}
+
+void alzaBraccio(){
+  servoMuoviBraccio.write(50);    //ALZATO
+  delay(1000);
+}
+
+void abbassaBraccio(){
+  servoMuoviBraccio.write(105);      //ABBASSATO
   delay(1000);
 }
